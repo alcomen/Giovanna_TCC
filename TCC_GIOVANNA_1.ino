cbcc1667da21eac4a7c177316f5280409534bc3c
alcomen <alcomen@gmail.com>
2020-10-02 12:54:47 -0300

    Add files via upload

---
+++ TCC_GIOVANNA_1.ino
@@ -0,0 +1,197 @@
+#include <Wire.h>
+#include <VL53L0X.h>
+#include <ADXL345.h>
+#include "gps.h"
+#include "wifi.h"
+
+//variáveis para caso de cadeiras com motor
+//const int motorA1  = 9;    // Pin  5 of L293.
+//const int motorA2  = 3;    // Pin  6 of L293.
+//int eixo_X= 34;//A0; //PINO REFERENTE A LIGAÇÃO DO EIXO X
+//int eixo_Y = 35;//A1; //PINO REFERENTE A LIGAÇÃO DO EIXO Y
+//int botao = 2; //PINO REFERENTE A LIGAÇÃO NO PINO KEY (EIXO Z)
+
+VL53L0X sensor;
+int i = 0;
+int j = 0;
+int state_rec;
+char state;
+
+ADXL345 adxl; //variable adxl is an instance of the ADXL345 library
+
+int valor_ax_anterior;
+int valor_ay_anterior;
+int valor_az_anterior;
+int valor_ax_atual;
+int valor_ay_atual;
+int valor_az_atual;
+int diferenca_ax;
+int diferenca_ay;
+int diferenca_az;
+
+int velocidade;
+char velocidade_texto [32];
+char msg[10];
+char loc[25];
+
+char d;
+
+void setup() 
+{
+  Serial.begin(115200);
+  setupGPS();
+
+  wifi_connect();
+  initMQTT();
+    
+  pinMode (12, INPUT_PULLUP);
+  digitalWrite (12, HIGH);
+
+  Wire.begin();
+  sensor.init();
+  sensor.setTimeout(500);
+  sensor.startContinuous();
+
+  adxl.powerOn();
+    //set activity/ inactivity thresholds (0-255)
+  adxl.setActivityThreshold(75); //62.5mg per increment
+  adxl.setInactivityThreshold(75); //62.5mg per increment
+  adxl.setTimeInactivity(10); // how many seconds of no activity is inactive?
+ 
+  //look of activity movement on this axes - 1 == on; 0 == off 
+  adxl.setActivityX(1);
+  adxl.setActivityY(1);
+  adxl.setActivityZ(1);
+ 
+  //look of inactivity movement on this axes - 1 == on; 0 == off
+  adxl.setInactivityX(1);
+  adxl.setInactivityY(1);
+  adxl.setInactivityZ(1);
+ 
+  //look of tap movement on this axes - 1 == on; 0 == off
+  adxl.setTapDetectionOnX(0);
+  adxl.setTapDetectionOnY(0);
+  adxl.setTapDetectionOnZ(1);
+ 
+  //set values for what is a tap, and what is a double tap (0-255)
+  adxl.setTapThreshold(50); //62.5mg per increment
+  adxl.setTapDuration(15); //625us per increment
+  adxl.setDoubleTapLatency(80); //1.25ms per increment
+  adxl.setDoubleTapWindow(200); //1.25ms per increment
+ 
+  //set values for what is considered freefall (0-255)
+  adxl.setFreeFallThreshold(7); //(5 - 9) recommended - 62.5mg per increment
+  adxl.setFreeFallDuration(45); //(20 - 70) recommended - 5ms per increment
+ 
+  //setting all interrupts to take place on int pin 1
+  //I had issues with int pin 2, was unable to reset it
+  adxl.setInterruptMapping( ADXL345_INT_SINGLE_TAP_BIT,   ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_DOUBLE_TAP_BIT,   ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_FREE_FALL_BIT,    ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,     ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_INACTIVITY_BIT,   ADXL345_INT1_PIN );
+ 
+  //register interrupt actions - 1 == on; 0 == off  
+  adxl.setInterrupt( ADXL345_INT_SINGLE_TAP_BIT, 1);
+  adxl.setInterrupt( ADXL345_INT_DOUBLE_TAP_BIT, 1);
+  adxl.setInterrupt( ADXL345_INT_FREE_FALL_BIT,  1);
+  adxl.setInterrupt( ADXL345_INT_ACTIVITY_BIT,   1);
+  adxl.setInterrupt( ADXL345_INT_INACTIVITY_BIT, 1);
+}
+
+void loop() 
+{
+  if (!mqtt.connected()) {
+    connectMQTT();
+  }
+  mqtt.loop();
+
+  //LIDAR
+  int distance =sensor.readRangeContinuousMillimeters();
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.print("mm");
+
+  if (distance > 300)
+  {
+    Serial.print("Perigo de desnível - travamento de rodas");
+    mqtt.publish("/tcc/distancia", "Perigo de desnível - travamento de rodas");
+  }
+
+  if (sensor.timeoutOccurred()) 
+  { 
+    Serial.print(" TIMEOUT"); 
+  }
+
+  //gps
+  updateGPS();
+
+  sprintf(msg, "%d", gps.satellites.value());
+  mqtt.publish("/giovanna/gps/sats", msg);
+
+  sprintf(loc, "%2.6f\,%2.6f", gps.location.lat(), gps.location.lng());
+  mqtt.publish("/giovanna/gps/loc", loc);
+  
+  velocidade = gps.speed.mps();
+  sprintf(msg, "%d", velocidade);
+  mqtt.publish("/giovanna/gps/vel", msg);
+
+  //accelerometro
+ int x,y,z;  
+  adxl.readXYZ(&x, &y, &z); //read the accelerometer values and store them in variables  x,y,z
+  // Output x,y,z values 
+  Serial.print("values of X , Y , Z: ");
+  Serial.print(x);
+  Serial.print(" , ");
+  Serial.print(y);
+  Serial.print(" , ");
+  Serial.println(z);
+  
+  double xyz[3];
+  double ax,ay,az;
+  adxl.getAcceleration(xyz);
+  ax = xyz[0];
+  ay = xyz[1];
+  az = xyz[2];
+  Serial.print("X=");
+  Serial.print(ax);
+  Serial.println(" g");
+  Serial.print("Y=");
+  Serial.print(ay);
+  Serial.println(" g");
+  Serial.print("Z=");
+  Serial.print(az);
+  Serial.println(" g");
+  Serial.println("**********************");
+
+  valor_ax_anterior = valor_ax_atual;
+  valor_ax_atual = ax;
+  diferenca_ax = valor_ax_atual - valor_ax_anterior;
+  if (diferenca_ax > 100)
+  {
+    mqtt.publish("/tcc/acelerometro_x", "possível colisão");
+  }
+
+  valor_ay_anterior = valor_ay_atual;
+  valor_ay_atual = ay;
+  diferenca_ay = valor_ay_atual - valor_ay_anterior;
+  if (diferenca_ay > 100)
+  {
+    mqtt.publish("/tcc/acelerometro_y", "possível colisão");
+  }
+
+  valor_az_anterior = valor_az_atual;
+  valor_az_atual = az;
+  diferenca_az = valor_az_atual - valor_az_anterior;
+  if (diferenca_az > 100)
+  {
+    mqtt.publish("/tcc/acelerometro_z", "possível colisão");
+  }
+
+  //gps
+  velocidade = gps.speed.value();
+  sprintf(velocidade_texto, "%d", velocidade);
+  mqtt.publish("/tcc/velocidade_gps", velocidade_texto);
+
+delay(1000);  
+}
